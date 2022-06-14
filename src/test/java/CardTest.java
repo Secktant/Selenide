f88db501c16d5c4ce00a9c190190f0cb9bce8a73
Nikita Santalov <na.santalov@yandex.ru>
2022-06-14 19:27:35 +0400

    fix

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,16 +1,7 @@
-
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
-
-import java.util.List;
-
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardTest {
 
