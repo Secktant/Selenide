c4250ed6f594567d1288ac804212dbdb41dc1f5c
Nikita Santalov <na.santalov@yandex.ru>
2022-06-14 17:06:54 +0400

    fix driver

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -27,7 +27,7 @@ public class CardTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
