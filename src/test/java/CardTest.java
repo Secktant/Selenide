e5b11d3dbc60c4b6c9340af5a00f70a171da3737
Nikita Santalov <na.santalov@yandex.ru>
2022-06-16 13:57:39 +0400

    fix test

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,8 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardTest {
 
@@ -31,15 +30,36 @@ public class CardTest {
     }
 
 
-//    @Test
-//    public void validationOfThePhoneField() {
-//        open("http://localhost:9999/");
-//        $("[type=text]").setValue("Никита");
-//        $("[type=tel]").setValue("79012345678");
-//        $(".checkbox__box").click();
-//        $(".button__content").click();
-//        $(".input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-//
-//    }
+    @Test
+    public void validationOfThePhoneField() {
+        open("http://localhost:9999/");
+        $("[type=text]").setValue("Никита");
+        $("[type=tel]").setValue("79012345678");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+    }
+
+    @Test
+    public void EmptyName() {
+        open("http://localhost:9999/");
+        $("[type=tel]").setValue("+79012345678");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void EmptyPhone() {
+        open("http://localhost:9999/");
+        $("[type=text]").setValue("Никита");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+
 
 }
+
